5f98d3c888e666b00bde52914d7e016729bd48fe
Emilia Zysk <emzysk.dev@gmail.com>
2023-02-08 20:53:19 +0900

    notes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 let age: number = 20;
 
+// tsc file_name.ts
+// tsc -init (=> tsconfig.json)
+// tsc => to compile
+
 // //debugging practice
 // if (age < 50) 
 //     age += 10;
 
-// types practice
+// //types practice
+// normal JS types + any, unknown, never, enum, tuple
