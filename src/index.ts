a7e85d8109cb286d6b17f92e14a51ecf953340ac
Emilia Zysk <emzysk.dev@gmail.com>
2023-02-08 22:11:54 +0900

    touple enum

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,13 @@
 
 // arrays
 // let numbers: number[] = [1,2,3];
+
+// touple
+// let user: [number, string] = [1,"Emilia"]
+
+//enum
+//pascal convention
+// enum Size { Small = 's', Medium = 'm', Large = 'l' }
+// enum Size { Small = 1, Medium, Large }
+// const enum Size { Small = 1, Medium, Large }
+// let mySize: Size = Size.Small;
