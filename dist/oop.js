e386e47ab510af8f27c898efdba2414d67d4853f
Emilia Zysk <emzysk.dev@gmail.com>
2023-03-01 16:52:06 +0900

    access control keywords

--- dist/oop.js
+++ dist/oop.js
@@ -3,14 +3,25 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(ammount) {
         if (ammount <= 0)
             throw new Error("invalid ammount");
-        this.balance += ammount;
+        this._balance += ammount;
+    }
+    getTax() {
+        let tax = this.calculateTax();
+        return tax;
+    }
+    getBalance() {
+        return this._balance;
+    }
+    calculateTax() {
+        return this._balance * .8;
     }
 }
 let account = new Account(1, "Em", 0);
 console.log(account instanceof Account);
+console.log(account.getBalance());
 //# sourceMappingURL=oop.js.map
--- src/oop.ts
+++ src/oop.ts
@@ -1,21 +1,38 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nichckname?: string;
 
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
 
     deposit(ammount: number): void {
         if (ammount <= 0)
             throw new Error("invalid ammount");
-        this.balance += ammount;
+        this._balance += ammount;
+    }
+    
+    getTax(): number {
+        let tax = this.calculateTax();
+        return tax;
+    }
+
+    getBalance(): number {
+        return this._balance
+    }
+
+    private calculateTax(): number {
+        return this._balance * .8;
     }
 }
 
 let account = new Account(1, "Em", 0); 
 console.log(account instanceof Account);
+
+// Access control keywords
+// Access modifiers: public, private, protected
+console.log(account.getBalance());
