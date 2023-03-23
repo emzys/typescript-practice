63bc9abe60d12e54df15f033701979617a79f062
Emilia Zysk <emzysk.dev@gmail.com>
2023-03-23 17:14:14 +0900

    generic classes

--- src/generics.ts
+++ src/generics.ts
@@ -1,8 +1,9 @@
 // Generic classes
 
-class KeyValuePair<T> {
-    constructor(public key: T, public value: string) {}
+class KeyValuePair<K, V> {
+    constructor(public key: K, public value: V) {}
 }
 
-// let pair = new KeyValuePair<number>(1,"hi");
-let pair = new KeyValuePair<string>("1","hi");
+// let pair = new KeyValuePair<number, string>(1,"hi");
+// let pair = new KeyValuePair<string, string>("1","hi");
+let pair = new KeyValuePair("1","hi"); // compiler will infer the type
